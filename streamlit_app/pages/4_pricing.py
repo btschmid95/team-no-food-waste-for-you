542a61ad62d247673880ff0b95df59d5fb12843b
btschmid95 <btschmid@umich.edu>
2025-10-24 19:08:53 -0400

    Scraped Recipes
    Added scraped logic and pulled recipes

--- streamlit_app/pages/4_pricing.py
+++ streamlit_app/pages/4_pricing.py
@@ -3,12 +3,30 @@ import pandas as pd
 import os
 
 st.set_page_config(page_title="Pricing", layout="wide")
+
+st.title("Pricing")
 st.write("Trader Joe's Products")
 
-csv_path = os.path.join("..", "web_scraper", "trader_joes_products.csv")
+csv_path = "../web_scraper/trader_joes_products.csv"
 
 try:
     df = pd.read_csv(csv_path)
-    st.dataframe(df,use_container_width=True, hide_index=True)
+
+    if "Link" in df.columns:
+        df = df.drop(columns=["Link"])
+
+    st.dataframe(
+        df,
+        use_container_width=True,
+        hide_index=True,
+        height=800,
+        column_config={
+            "url": st.column_config.LinkColumn(
+                "Product URL",
+                help="Click to view the Trader Joe's product page"
+            )
+        }
+    )
+
 except FileNotFoundError:
     st.error(f"File not found at {csv_path}")
